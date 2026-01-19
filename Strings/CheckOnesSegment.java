6f0b9d9f35a2d198c81adf3186e8fe8a5a0c2e08
Anunay Kumar <anunay5832@gmail.com>
2026-01-19 20:56:02 +0530

    Create CheckOnesSegment.java

---
+++ Strings/CheckOnesSegment.java
@@ -0,0 +1,7 @@
+// LeetCode 1784 - Check if Binary String Has at Most One Segment of Ones
+
+class Solution {
+    public boolean checkOnesSegment(String s) {
+        return !s.contains("01");
+    }
+}
