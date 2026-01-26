8c12cfd089f3e3805fc9429ae1706efede16b634
Anunay Kumar <anunay5832@gmail.com>
2026-01-26 18:33:00 +0530

    Create Solution.java

---
+++ Arrays/minimum-absolute-difference/Solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        int n = arr.length;
+        List<List<Integer>> ans = new ArrayList<>();
+
+        Arrays.sort(arr);
+
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 1; i < n; i++) {
+            min = Math.min(min, arr[i] - arr[i - 1]);
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] - arr[i - 1] == min) {
+                List<Integer> pair = new ArrayList<>();
+                pair.add(arr[i - 1]);
+                pair.add(arr[i]);
+                ans.add(pair);
+            }
+        }
+
+        return ans;
+    }
+}
