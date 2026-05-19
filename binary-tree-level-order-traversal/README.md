e7b5a7c0de5b5624d7aff7e841eebe6323a29a6f
Your Name <you@example.com>
2026-05-19 12:00:00 +0000

    Day 6: Binary Tree Level Order Traversal

---
+++ binary-tree-level-order-traversal/README.md
@@ -0,0 +1,18 @@
+# Day 6: Binary Tree Level Order Traversal
+
+**Difficulty:** Medium
+
+## Problem
+
+Given the root of a binary tree, return the level order traversal of its nodes' values (left to right, level by level).
+
+## Example
+
+```
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+```
+
+## Files
+
+- `Solution.java` - Java solution stub for this problem
---
+++ binary-tree-level-order-traversal/Solution.java
@@ -0,0 +1,22 @@
+// Definition for a binary tree node.
+// public class TreeNode {
+//     int val;
+//     TreeNode left;
+//     TreeNode right;
+//     TreeNode() {}
+//     TreeNode(int val) { this.val = val; }
+//     TreeNode(int val, TreeNode left, TreeNode right) {
+//         this.val = val;
+//         this.left = left;
+//         this.right = right;
+//     }
+// }
+
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        // TODO: implement your solution here
+        return null;
+    }
+}
