cc157976fd4d67c091e0cd9b6e849804c85b4c4d
Your Name <you@example.com>
2026-05-24 12:00:00 +0000

    Day 11: Path Sum II

---
+++ path-sum-ii/README.md
@@ -0,0 +1,18 @@
+# Day 11: Path Sum II
+
+**Difficulty:** Medium
+
+## Problem
+
+Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where the sum of the node values equals targetSum.
+
+## Example
+
+```
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: [[5,4,11,2],[5,8,4,5]]
+```
+
+## Files
+
+- `Solution.java` - Java solution stub for this problem
---
+++ path-sum-ii/Solution.java
@@ -0,0 +1,22 @@
+// Definition for a binary tree node.
+// public class TreeNode {
+//     int val;
+//     TreeNode left;
+//     TreeNode right;
+//     TreeNode() {}
+//     TreeNode(int val) { this.val = val; }
+//     TreeNode(int val, TreeNode left, TreeNode right) {
+//         this.val = val;
+//         this.left = left;
+//         this.right = right;
+//     }
+// }
+
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        // TODO: implement your solution here
+        return null;
+    }
+}
