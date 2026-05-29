dd83fdf31f315114cec7b918def5b6a405aba3da
Your Name <you@example.com>
2026-05-29 12:00:00 +0000

    Day 16: Serialize and Deserialize Binary Tree

---
+++ serialize-and-deserialize-binary-tree/README.md
@@ -0,0 +1,18 @@
+# Day 16: Serialize and Deserialize Binary Tree
+
+**Difficulty:** Hard
+
+## Problem
+
+Design an algorithm to serialize a binary tree to a string and deserialize that string back to the original tree structure.
+
+## Example
+
+```
+Input: root = [1,2,3,null,null,4,5]
+Output: [1,2,3,null,null,4,5]
+```
+
+## Files
+
+- `Solution.java` - Java solution stub for this problem
---
+++ serialize-and-deserialize-binary-tree/Solution.java
@@ -0,0 +1,27 @@
+// Definition for a binary tree node.
+// public class TreeNode {
+//     int val;
+//     TreeNode left;
+//     TreeNode right;
+//     TreeNode() {}
+//     TreeNode(int val) { this.val = val; }
+//     TreeNode(int val, TreeNode left, TreeNode right) {
+//         this.val = val;
+//         this.left = left;
+//         this.right = right;
+//     }
+// }
+
+class Codec {
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        // TODO: implement your solution here
+        return "";
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        // TODO: implement your solution here
+        return null;
+    }
+}
