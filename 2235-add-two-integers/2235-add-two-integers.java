55249f68abff75a4feff382d470ba8ee5a94ce22
Anunay Kumar <anunay5832@gmail.com>
2026-08-17 12:15:38 +0530

    Time: 0 ms (100%), Space: 42 MB (88.38%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.java
+++ 2235-add-two-integers/2235-add-two-integers.java
@@ -1,6 +1,5 @@
 class Solution {
     public int sum(int num1, int num2) {
-        int beryberylargekljerwrsfkljjhh = num1+num2;
-        return beryberylargekljerwrsfkljjhh;
+        return num1 + num2;
     }
 }
